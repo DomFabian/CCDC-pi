9fc006b172f6094412fbe29a99ef3a244cc741fa
DomFabian <dominickfabian@gmail.com>
2018-07-02 16:16:49 +0000

    Move key out of repo

--- binary/file_sender.sh
+++ binary/file_sender.sh
@@ -6,6 +6,6 @@
 ## 10.10.10.10: motor-driving Raspberry Pi
 ## 10.10.10.11: logic puzzle/binary sequence Raspberry Pi
 
-sshpass -f "/home/pi/CCDC-pi/key" ssh 10.10.10.11 touch /home/pi/CCDC-pi/binary/binary_soln
+sshpass -f "/home/pi/key" ssh 10.10.10.11 touch /home/pi/CCDC-pi/binary/binary_soln
 
 exit 0
